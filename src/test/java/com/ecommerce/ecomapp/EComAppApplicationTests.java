24375c85c7ac96229e17b32722161930f7db9d3f
Akhil Krishnan <akhilunnikrishnan933@gmail.com>
2024-08-04 18:27:12 +0530

    Initialized Springboot project using Spring initializer

---
+++ src/test/java/com/ecommerce/ecomapp/EComAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
