59cdc3d8a35d7aac3cce9c79d62b6e6410bed646
Akhil Krishnan <akhilunnikrishnan933@gmail.com>
2024-08-15 20:34:43 +0530

    Enabled JPAAuditing and EntityListener for created and lastupdated fields

--- src/main/java/com/ecommerce/ecomapp/models/BaseModel.java
+++ src/main/java/com/ecommerce/ecomapp/models/BaseModel.java
@@ -1,19 +1,26 @@
 package com.ecommerce.ecomapp.models;
 
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import jakarta.persistence.*;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseModel {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @CreatedDate
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date createdAt;
+    @LastModifiedDate
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date lastUpdatedAt;
     private boolean isDeleted;
 }
